595dadeb11de5ca918b8e8e11cd1a4d1837365c9
Jago de Vreede <jagodevreede@gmail.com>
2025-05-09 21:30:28 +0200

    STEP 9: combo box with qute

--- src/main/java/io/github/jagodevreede/demo/nohassle/text/MemeTextResource.java
+++ src/main/java/io/github/jagodevreede/demo/nohassle/text/MemeTextResource.java
@@ -1,5 +1,7 @@
 package io.github.jagodevreede.demo.nohassle.text;
 
+import io.github.jagodevreede.demo.nohassle.image.MemeImage;
+import io.github.jagodevreede.demo.nohassle.image.MemeImageRepository;
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
 import jakarta.inject.Inject;
@@ -7,7 +9,6 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriBuilder;
-import org.jboss.logging.BasicLogger;
 
 import java.util.List;
 import java.util.UUID;
@@ -21,11 +22,22 @@ public class MemeTextResource {
     @Inject
     MemeTextRepository memeTextRepository;
 
+    // Simplicity for demo
+    @Inject
+    MemeImageRepository memeImageRepository;
+
     @GET
     @Path("")
     public TemplateInstance listAll() {
         List<MemeText> memes = memeTextRepository.findAll();
         return Templates.table(memes);
+    }
+
+    @GET
+    @Path("/comboBox")
+    public TemplateInstance comboBox() {
+        List<MemeImage> images = memeImageRepository.findAll();
+        return Templates.comboBox(images);
     }
 
     @GET
@@ -46,7 +58,6 @@ public class MemeTextResource {
                                @FormParam("imageId") String imageId) {
         MemeText meme = new MemeText(UUID.randomUUID(), upperText, lowerText, imageId);
         memeTextRepository.save(meme);
-        final BasicLogger logger;
         LOG.info("Meme created: " + meme);
         // Redirect to the main page with a success message in the query params
         return Response.seeOther(UriBuilder.fromPath("index.html").queryParam("success", "true").build()).build();
@@ -57,5 +68,7 @@ public class MemeTextResource {
         public static native TemplateInstance table(List<MemeText> memes);
 
         public static native TemplateInstance detail(MemeText meme);
+        public static native TemplateInstance comboBox(List<MemeImage> images);
+
     }
 }
